f34ff3b00ec71c3dddc23ff1e991fef2395c2d54
JohnCarlo001 <136680565+JohnCarlo001@users.noreply.github.com>
2023-06-15 14:48:54 +0800

    Initialize

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-layout view="hhh lpR lfr">
+
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="header">
+            <div class="column">
+              First Column
+            </div>
+            <div class="column" style="flex: 2;">
+              <q-input outlined dense placeholder="Search">
+                <template v-slot:prepend>
+                  <q-icon name="search" />
+                </template>
+              </q-input>
+            </div>
+            <div class="column">
+              <img src="src/assets/cart-icon.PNG" alt="" style="width: 50px;">
+            </div>
+          </div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<style>
+.header {
+  background-color: rgb(48, 174, 216);
+  display: flex;
+  align-items: center;
+  padding: 10px;
+}
+
+.column {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,73 @@
+<!-- <template>
+  <q-page class="container">
+    <title>Login</title>
+    <form>
+      <label for="username">Username</label>
+      <input type="text" id="username" name="username" placeholder="Enter your username">
+
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" placeholder="Enter your password">
+     
+
+      <input type="submit" value="Login">
+    </form>
+  </q-page>
+</template>
+<style>
+body {
+  background-color: #7470e6;
+  font-family: Arial, sans-serif;
+
+}
+
+.container {
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #4cb783;
+  border: 1px solid #cccccc;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #cccccc;
+  border-radius: 3px;
+}
+
+input[type="submit"] {
+  width: 100%;
+  padding: 10px;
+  background-color: #ee855f;
+  color: #ffffff;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #45a049;
+}
+</style> -->
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
